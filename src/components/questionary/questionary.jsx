05b540bff1c37e9b09ba955e693b6441c01e1861
lucass2k <lucasnahuel2000silva@gmail.com>
2023-11-02 20:32:53 -0300

    wrong and correct response

--- src/components/questionary/questionary.jsx
+++ src/components/questionary/questionary.jsx
@@ -1,31 +1,60 @@
 import { questions } from "../../data/data";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   StyledQuestionary,
   StyledAnswer,
   StyledQuestion,
 } from "./questionaryStyles";
+
 const Questionary = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
 
   const currentQuestion = questions[currentQuestionIndex];
 
   const handleAnswerClick = (selectedAnswer) => {
     if (selectedAnswer === currentQuestion.correctAnswer) {
+      setIsAnswerCorrect(true);
       setScore(score + 1);
-    }
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
     } else {
+      setIsAnswerCorrect(false);
     }
   };
+
+  useEffect(() => {
+    if (isAnswerCorrect !== null) {
+      const delay = setTimeout(() => {
+        if (currentQuestionIndex < questions.length - 1) {
+          setCurrentQuestionIndex(currentQuestionIndex + 1);
+        } else {
+        }
+        setIsAnswerCorrect(null);
+      }, 1000);
+
+      return () => clearTimeout(delay);
+    }
+  }, [isAnswerCorrect, currentQuestionIndex]);
+
   return (
     <StyledQuestionary>
-      <div>Actual score: {score}</div>
+      <div>Puntaje actual: {score}</div>
       <StyledQuestion>{currentQuestion.question}</StyledQuestion>
       {currentQuestion.answers.map((answer, index) => (
-        <StyledAnswer key={index} onClick={() => handleAnswerClick(answer)}>
+        <StyledAnswer
+          key={index}
+          onClick={() => handleAnswerClick(answer)}
+          style={{
+            backgroundColor:
+              isAnswerCorrect === true &&
+              answer === currentQuestion.correctAnswer
+                ? "green"
+                : isAnswerCorrect === false &&
+                  answer === currentQuestion.correctAnswer
+                ? "red"
+                : "white",
+          }}
+        >
           {answer}
         </StyledAnswer>
       ))}
